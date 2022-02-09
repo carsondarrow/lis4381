fee5713949fb516a0288132c7b639b83db9ba484
cbd19a <cbd19a@my.fsu.edu>
2022-02-09 15:27:08 -0500

    changed main READ.ME

--- skillset/4_decision_structures/Methods.java
+++ skillset/4_decision_structures/Methods.java
@@ -7,8 +7,8 @@ public class Methods
    {
     System.out.println("Developer: Carson Darrow");
     System.out.println("Program evaluates user-entered characters.");
-    System.out.println("Use following characters: W or w, C or c, H or h, N or n.")
-    System.out.println("Use following decision structures: if...else, and switch.")
+    System.out.println("Use following characters: W or w, C or c, H or h, N or n.");
+    System.out.println("Use following decision structures: if...else, and switch.");
 
     System.out.println();
 }
@@ -21,12 +21,12 @@ public static void getUserPhoneType()
     Scanner sc = new Scanner(System.in);
 
 
-    System.out.println("Phone types: W or w (work), C or c (cell), H or h (home), N or n (none).")
-    System.out.println("Enter phone type: ")
+    System.out.println("Phone types: W or w (work), C or c (cell), H or h (home), N or n (none).");
+    System.out.println("Enter phone type: ");
     myStr = sc.next().toLowerCase();
     myChar = myStr().charAt(0);
     
-    System.out.println("\nif...else:")
+    System.out.println("\nif...else:");
 
     if (myChar == 'w')
         System.out.println("Phone type: work");
@@ -41,19 +41,19 @@ public static void getUserPhoneType()
 
 
     System.out.println();
-    System.out.println("switch: ")
+    System.out.println("switch: ");
     switch (myChar)
     {
     case 'w':
-        System,out.println("Phone type: work");
+        System.out.println("Phone type: work");
         break;
     case 'c':
         System.out.println("Phone type: cell");
         break;
-    case 'h';
+    case 'h':
         System.out.println("Phone type: home");
         break;
-    case 'n'
+    case 'n':
         System.out.println("Phone type: none");
         break;
     default:
