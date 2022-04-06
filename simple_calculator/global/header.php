3dcbd39174cc4645419bcca01633a2bc0e8349f6
cbd19a <cbd19a@my.fsu.edu>
2022-04-06 16:58:11 -0400

    change main READ.ME

---
+++ simple_calculator/global/header.php
@@ -0,0 +1,14 @@
+<style>
+h1
+{
+text-shadow: 2px 2px #788585;
+}
+h1,h3
+{
+color: #000;
+text-align: center;
+}
+</style>
+
+<h1>Simple Calculator</h1>
+<h3>Performs Addition, Subtraction, Multiplication, and Division</h3>
