caf41f7094567c59207410f78aae518d619030c2
cbd19a <cbd19a@my.fsu.edu>
2022-08-23 15:08:00 -0400

    change main READ.ME

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 		<meta name="author" content="Carson Darrow">
 		<link rel="icon" href="favicon.ico">
 
-		<title>Creative Online Portfolio TEST</title>
+		<title>Creative Online Portfolio</title>
 
 		<?php include_once("css/include_css.php"); ?>	
 
